e226a3842d641c532eb0d6c28e53295154633ad7
Ryder Hutchings <150638428+ryderhutchings@users.noreply.github.com>
2026-04-19 21:36:10 -0600

    Update run-sbc-bench.sh

--- run-sbc-bench.sh
+++ run-sbc-bench.sh
@@ -4,6 +4,9 @@ set -e
 BASE="$HOME/Benchy"
 RESULTS_FILE="$BASE/sbc-bench-results.txt"
 
+COMPLETED_DIR="$BASE/DONE"
+mkdir -p "$COMPLETED_DIR"
+
 mkdir -p "$BASE"
 
 echo "[*] Running sbc-bench..."
@@ -12,3 +15,5 @@ sudo /bin/bash "$BASE/sbc-bench.sh" -r 2>&1 | tee "$RESULTS_FILE"
 
 echo ""
 echo "[*] Results saved to $RESULTS_FILE"
+
+mv "$(realpath "$0")" "$COMPLETED_DIR/"
