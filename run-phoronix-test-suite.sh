3949185021d28faa9e2c622ae8a23cb4fba288b7
Ryder Hutchings <150638428+ryderhutchings@users.noreply.github.com>
2026-04-19 20:11:07 -0600

    Update run-phoronix-test-suite.sh

--- run-phoronix-test-suite.sh
+++ run-phoronix-test-suite.sh
@@ -17,3 +17,17 @@ sudo bash "$BASE/sbc-general-benchmark/sbc-general-benchmark.sh" -r 2>&1 | tee "
 
 echo ""
 echo "[✓] Results saved to $RESULTS_FILE"
+
+# If you see:
+# [*] Running sbc-general-benchmark (PTS wrapper)...
+# Reading package lists...
+# Building dependency tree...
+# Reading state information...
+# E: Unable to locate package php8.2-cli
+# E: Couldn't find any package by glob 'php8.2-cli'
+# E: Couldn't find any package by regex 'php8.2-cli'
+# E: Unable to locate package php8.2-xml
+# E: Couldn't find any package by glob 'php8.2-xml'
+# E: Couldn't find any package by regex 'php8.2-xml'
+#
+# Do this: bash PHP_VERSION="8.4" run-phoronix-test-suite.sh
